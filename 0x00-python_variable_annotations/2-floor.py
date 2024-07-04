3feb48adb2a07f283d8101bd151db9b776cb03ed
Azara <yyushahu02@gmail.com>
2024-07-04 13:55:15 +0000

    type annotated function that floors a float value and returns the floored value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Type-Annotated function: floor()
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-Annotated function: floor().
+
+    Takes an argument of type float, floors it and returns the floored
+    argument as an int.
+
+    Args:
+            n (float): Float argument to be floored
+
+    Returns:
+            Floor value of n (int).
+    """
+    return int(n)
