4a7283cc8a14a7ff1bdb40e5be8ed17344c06f24
Azara <yyushahu02@gmail.com>
2024-07-04 13:44:47 +0000

    concat two strings and return the concatenated string as a string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Type-Annotated function: concat()
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-Annotated function: concat().
+
+    Takes two arguments as strings, concatenates them and
+    returns them as a single, typed string.
+
+    Args:
+            str1 (str): First string argument.
+            str2 (str): Second string argument.
+
+    Returns:
+            A concatenated string.
+    """
+    return str1 + str2
