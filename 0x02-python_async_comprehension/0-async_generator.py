605feb4d06ae132eb405dd05ec8237fd94ca22f5
Azara <yyushahu02@gmail.com>
2024-07-09 11:50:41 +0000

    python - async comprehension: init commit with an async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+from typing import AsyncGenerator
+import asyncio
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    An asynchronous generator that yields a random number between 0 and 10
+    after asynchronously waiting for 1 second, a total of 10 times.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,2 @@
+# Python - Async Comprehension
+This directory contains advanced python code for async list, set, dict comprehension.
