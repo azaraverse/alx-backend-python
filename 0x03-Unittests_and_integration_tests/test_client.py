929a5dd19818d0c05a71229f66cb97427245114a
Azara <yyushahu02@gmail.com>
2024-08-02 20:14:39 +0000

    remove type annotations for test_has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -109,9 +109,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_license(
-        self, repo, license_key: str, expected: bool
-    ) -> Any:
+    def test_has_license(self, repo, license_key, expected):
         """ Tests that the has_license method returns the expected value
         by parameterizing the test with inputs
         """
