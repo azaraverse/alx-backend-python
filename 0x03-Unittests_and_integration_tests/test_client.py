da6b1cf16b3c89ffa1a34982624c2534fe38e51c
Azara <yyushahu02@gmail.com>
2024-08-02 20:15:43 +0000

    change the org_name of the client class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -113,7 +113,7 @@ class TestGithubOrgClient(unittest.TestCase):
         """ Tests that the has_license method returns the expected value
         by parameterizing the test with inputs
         """
-        client = GithubOrgClient("test_org")
+        client = GithubOrgClient("gitloper-azara")
         result = client.has_license(repo, license_key)
 
         self.assertEqual(result, expected)
